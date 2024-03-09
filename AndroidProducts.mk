c019873a51be65057a125f66195d1ba069c18c81
Ni20110118 <153351536+Ni20110118@users.noreply.github.com>
2024-03-09 19:32:03 +0800

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_mh2lm.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_mh2lm-user \
+    omni_mh2lm-userdebug \
+    omni_mh2lm-eng
